e65ddc52184ff1cd7eab9b1b20180398514171d3
Iurikman <y.m.kutyrev@gmail.com>
2022-11-25 12:55:02 +0300

    1 commit

---
+++ Ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b) Console.WriteLine("числа равны");
+else 
+{
+if (a>b) Console.WriteLine("число 1 болье числа 2");
+else Console.WriteLine("число 2 болье числа 1");
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 3");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b>max && b>c) max = b;
+else 
+{
+    if (c>a) max = c;
+}
+Console.WriteLine($"{max}");
