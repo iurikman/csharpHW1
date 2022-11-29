de8f8149967f93133c4c9de3d73ec1c43d7c00fb
Iurikman <y.m.kutyrev@gmail.com>
2022-11-29 23:10:06 +0300

    29-11-2022_3

---
+++ Ex7/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+string Method(string text, char a, char b) {
+
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == a) result = result + $"{b}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string text = "аапппппппппппп";
+string ntext = Method(text, 'п', 'П');
+Console.WriteLine(ntext);
