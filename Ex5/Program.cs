893cc5d733f054b9b4d6153ed308435681f6fbf5
Iurikman <y.m.kutyrev@gmail.com>
2022-11-29 19:58:53 +0300

    29-11-2022

---
+++ Ex5/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+
+Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+             |
+      2      |       1
+             |
+-------------------------->
+             |
+      3      |       4
+*/
+
+//функция получения числа с консоли
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0 && result < 5)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите число четверт (от 1 до 4)");
+        }
+    }
+
+    return result;
+}
+
+//получение четверти, в которой лежит точка с координатами (x, y)
+
+string GetQuarter(int x)
+{
+    if(x == 1)
+        return "x > 0 && y > 0";
+    else if(x == 2)
+        return "x < 0 && y > 0";
+    else if(x == 3)
+        return "x < 0 && y < 0";
+    else
+        return "x > 0 && y < 0";
+}
+
+int x = GetNumber("Введите четверть:");
+
+string quarter = GetQuarter(x);
+
+Console.WriteLine($"{quarter}");
+
---
+++ Ex6/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите число четверт (от 1 до 4)");
+        }
+    }
+
+    return result;
+}
+
+
+int GetRes(int x) 
+{
+    int res = x*x;
+    return res;
+}
+
+int x = GetNumber("Введите число");
+
+int i = 1;
+while (i <= x) {
+    int rev = GetRes(i);
+    Console.Write($"{rev}, ");
+    i++;
+}
